8ac3510e509b08daf86dd449b193c74b27c67bf4
afeiship <1290657123@qq.com>
2022-06-29 10:19:47 +0800

    feat: rm .git

---
+++ generators/app/templates1/src/install.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         <%= project_name %>
+// @namespace    https://github.com/afeiship/<%= project_name %>
+// @version      __VERSION__
+// @description  <%= description %>
+// @author       afeiship
+// @match        *://*/*
+// @require  https://unpkg.com/@jswork/gm-sdk@1.0.21
+// @require  https://cdn.jsdelivr.net/npm/@jswork/<%= project_name %>@__VERSION__
+// @resource GM_STYLES https://unpkg.com/@jswork/gm-styles@1.1.3
+// @grant GM_addStyle
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_addValueChangeListener
+// @grant GM_removeValueChangeListener
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_log
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_registerMenuCommand
+// @grant GM_unregisterMenuCommand
+// @grant GM_openInTab
+// @grant GM_xmlhttpRequest
+// @grant GM_download
+// @grant GM_getTab
+// @grant GM_saveTab
+// @grant GM_getTabs
+// @grant GM_notification
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant  window.focus
+// @icon64 https://tva1.sinaimg.cn/large/007S8ZIlgy1gexw87htqhj305k05k74o.jpg
+// ==/UserScript==
